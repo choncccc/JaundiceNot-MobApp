9eb194ea82620b95d52be9c20168165270399c1f
kenj <kenjieramos22@gmail.com>
2025-03-12 01:54:20 +0800

    ""

--- app/src/main/java/com/example/jaundicednot/OnboardingFragment1.kt
+++ app/src/main/java/com/example/jaundicednot/OnboardingFragment1.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class OnboardingFragment1: Fragment() {
+    private lateinit var txtFAQ : TextView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.onboardingscreen1, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        txtFAQ = view.findViewById(R.id.txtFAQ)
+        txtFAQ.setOnClickListener{
+
+        }
     }
 }
